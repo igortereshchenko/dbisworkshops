b146097ea89cbbb216c5dd08c154d9b77c9af9ae
deepwebhoax <32373109+deepwebhoax@users.noreply.github.com>
2020-05-27 13:11:24 +0300

    Workshop 2 (3)

---
+++ Tyshchenko_Bohdan/WS2/routes.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, abort, request, redirect
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+	return render_template('welcome.html', time=datetime.now().strftime("%H:%M:%S"))
+
+@app.route('/api/<action>', methods=['POST', 'GET'])
+def action(action=None):
+	if action=="deputee":
+		return render_template('deputee.html', dictionary=deputee)
+	elif action=="project":
+		return render_template('project.html', dictionary=project)
+	elif action=="all":
+		if request.method=='POST':
+			result = request.form
+			if 'password' in list(result.keys()):
+				deputee['name'] = result['name']
+				deputee['face'] = result['face']
+				deputee['password'] = result['password']
+				deputee['head'] = result['head']
+				deputee['fraction'] = result['fraction']
+				deputee['mobile'] = result['mobile']
+			else:
+				project['name'] = result['name']
+				project['date_added'] = result['date_added']
+				project['document'] = result['document']
+				project['author'] = result['author']
+				project['deadline'] = result['deadline']
+			
+			return redirect(url_for('action', action="all"))
+		return render_template('all.html', dictProj=project, dictDep=deputee)
+	else:
+		abort(404)
+	
+	
+if __name__=='__main__':
+	deputee = { "name":"Микола Старицький",
+				"face":[],
+				"password":"******",
+				"head":1,
+				"fraction":"Свобода",
+				"mobile":"+380829562957"}
+	project = { "name":"Авторське право",
+				"date_added":"26.05.20",
+				"document":"law1.pdf",
+				"author":"Микола Старицький",
+				"deadline":"5.06.20"}
+	app.run(debug=True)
