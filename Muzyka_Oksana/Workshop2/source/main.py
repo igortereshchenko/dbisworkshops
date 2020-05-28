32b4864b2bcb6c339c1645f0411a629fdaea3230
Оксана Музика <32312185+oksanamuzyka@users.noreply.github.com>
2020-05-29 02:27:19 +0300

    Workshop2.MuzykaOksanaKM-73

---
+++ Muzyka_Oksana/Workshop2/source/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/api/<action>', methods = ['GET', 'POST'])
+def apiget(action):
+	if action == 'group':
+		return render_template('group.html', edition = groups)
+	elif action == 'customer':
+		return render_template('customer.html', customer = customers)
+	elif action == 'all':
+		return render_template('all.html', edition = groups, customer = customers)
+	else:
+		return render_template('404.html')
+
+groups =  {
+	"Name":"El-7",
+	"Level":"Elementary",
+	"Timetable":"Пн-Чт(19:35-21:15)",
+	"Teachers":"Anna L, Peter",
+	"Price":"1800"
+	}
+customers = {
+	"Name":"Бобич Боб Бобович",
+	"Age":"18" 
+	"Email":"boobb@mail.com",
+	"Phone":"0111111111"
+	}
+
+if __name__ == '__main__':
+	app.run(debug = True)
