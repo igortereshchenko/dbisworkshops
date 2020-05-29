dbca32f05f271fd5456680d2e3bccdbc3bd4fb92
Dmytro <zelenyid446@gmail.com>
2020-05-30 00:23:58 +0300

    Init workshop 4

---
+++ Zelenyi_Dmytro/workshop4/sourse/credentials.py
@@ -0,0 +1,20 @@
+import cx_Oracle
+
+username = 'zelenyid'
+password = 'oracle'
+ip = 'localhost'
+port = 1521
+service_name = 'XE'
+dsn = cx_Oracle.makedsn(ip, port, service_name=service_name)
+databaseName = dsn
+
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+USERNAME = 'zelenyid'
+PASSWORD = 'oracl'
+
+HOST = 'localhost'
+PORT = 1521
+SERVICE = 'orcl'
+
+ENGINE_PATH_WIN_AUTH = DIALECT + '+' + SQL_DRIVER + '://' + USERNAME + ':' + PASSWORD +'@' + HOST + ':' + str(PORT) + '/?service_name=' + SERVICE
