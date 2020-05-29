44e321e80e4f2da38c3634661c1ffe536b87b379
Mykyta Zviahin <35952133+nikitazviahin@users.noreply.github.com>
2020-05-29 13:26:35 +0300

    Create delete.js

---
+++ workshop5(lab6)/application/static/delete.js
@@ -0,0 +1,9 @@
+console.log('debug');
+// array html element
+
+const deleteBtns = document.querySelectorAll('delete-btn');
+deleteBtns.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    alert('DELETE!');
+});
+});
