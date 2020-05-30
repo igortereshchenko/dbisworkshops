176087dac3d7190077e154fe34b2df71168ea879
Yelue <31901392+Yelue@users.noreply.github.com>
2020-05-30 08:28:13 +0300

    Add files via upload

--- Smirnov_Artem/workshop5/source/main.py
+++ Smirnov_Artem/workshop5/source/main.py
@@ -35,8 +35,10 @@ ts = Worker(db)
 ts.delete_scores()
 ts.delete_recoms()
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     form_student = StudentForm(request.form)
     form_score = ScoreForm(request.form)
     form_search = SearchForm(request.form)
@@ -63,6 +65,7 @@ def index():
                     status='Added score'
                     return redirect(url_for('index'))
 
+
         elif request.form.get('submit_atestat')=='Add score':
             if session['student_id']:
                 if form_atestat.validate():
@@ -70,12 +73,10 @@ def index():
                     ts.add_score(form,int(session['student_id']))
                     return redirect(url_for('index'))
 
+
         elif request.form.get('submit_search')=='Search':
-            # print('in')
-            # print(form_search.validate())  
-            # print(form_search.errors)  
             if form_search.validate():
-                print(request.form['search_region'],type(request.form['search_region']))
+                
                 results = ts.make_recomms(request.form,session['student_id'])
                 if not results[0]:
                     status = results[1]
@@ -85,18 +86,17 @@ def index():
 
                 ts.add_recom_list(results,session['student_id'])
                 return redirect(url_for('index'))
-            else:
-                status = "fail search"
+            
     
 
     bar_graph = None 
-     
+    student_id=False
     if 'student_id' in session:
-        print(session['student_id'])
+        student_id=session['student_id']
         student_scores = ts.get_scores(session['student_id'])
         result_search = ts.get_recom_list(session['student_id'])
         if result_search:
-            print('inbargraph')
+            
             bar_graph = ts.make_plot(result_search) 
 
 
@@ -104,14 +104,15 @@ def index():
 
     
 
-    print(bar_graph)
+    print('student_id',student_id)
     return render_template('index.html', form_student=form_student, 
                                         form_score=form_score,
                                         result_search=result_search,
                                         student_scores=student_scores,
                                         form_search=form_search,
                                         form_atestat=form_atestat,
-                                        plot=bar_graph)
+                                        plot=bar_graph,
+                                        student_id=student_id)
 
 
 if __name__ == '__main__':
