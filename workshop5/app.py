1eaa82ec49c08a7eccaf5d9f04716b0080764385
Stas <61114611+StasBiruk@users.noreply.github.com>
2020-05-30 11:07:56 +0300

    add changes

--- workshop5/app.py
+++ workshop5/app.py
@@ -1,14 +1,29 @@
+import json
+import plotly
 import pickle
 import numpy as np
-from flask import Flask, render_template, redirect, url_for, request
+import plotly.graph_objs as go
 from flask_bootstrap import Bootstrap
-from sqlalchemy.orm import sessionmaker
-
+from flask import Flask, render_template, redirect, url_for, request
 from forms.wtf_form import  GetHealsParamForm
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'ThisIam'
+
+
+def create_pie(x1, x2):
+
+    labels = ['гірший','кращий'] 
+    values = [x1, x2]
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent',
+    	hole=.3, marker_colors=['rgb(0,255,255)','rgb(64,224,208)'])])
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
 
 
 
@@ -39,16 +54,18 @@ def form():
 		with open('ML/model', 'rb') as f:
 			model = pickle.load(f)
 
-		result = model.predict_proba(x.reshape(1, -1))
+		result = model.predict_proba(x.reshape(1, -1))[0][0]
 
-		return render_template('result.html', result=round(result[0][0], 3))
+		data = np.genfromtxt('ML/plotly_data.csv',delimiter=',')
+		x, y = np.sum(data[:, 0] > result), np.sum(data[:, 0] < result)
+		pie_graph = create_pie(x, y)
+
+
+		return render_template('result.html', result=round(result, 3), 
+			plot = pie_graph)
 
 	return render_template('index.html', form=form)
 
-
-@app.route('/result')
-def result():
-	return render_template('result.html')
 
 
 if __name__ == '__main__':
